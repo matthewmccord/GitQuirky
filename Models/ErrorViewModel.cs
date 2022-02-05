ac8c53f112053f8bdc56ee7e561e2494fb2be4c3
matthewmccord <matthewmccord3@gmail.com>
2022-02-05 12:17:24 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitQuirky.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
